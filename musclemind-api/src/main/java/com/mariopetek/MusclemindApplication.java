d57ca5444e7039d9aa463a8dd9b2d2dc5c9a4f2a
mariopetek <mariopetek0@gmail.com>
2023-03-27 22:53:56 +0200

    Spring application init

---
+++ musclemind-api/src/main/java/com/mariopetek/MusclemindApplication.java
@@ -0,0 +1,13 @@
+package com.mariopetek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusclemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusclemindApplication.class, args);
+	}
+
+}
