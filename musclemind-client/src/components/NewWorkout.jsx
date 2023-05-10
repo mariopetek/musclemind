20ae512dfa5f46c3ac17f7623fed0b2951ce2a6e
mariopetek <mariopetek0@gmail.com>
2023-05-10 20:45:59 +0200

    new workout form design progress

--- musclemind-client/src/components/NewWorkout.jsx
+++ musclemind-client/src/components/NewWorkout.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
+import { IconContext } from 'react-icons'
 import { SlLock, SlGlobe } from 'react-icons/sl'
+import { BsEmojiFrown } from 'react-icons/bs'
 
 import styles from '../styles/NewWorkout.module.css'
 
@@ -77,12 +79,16 @@ const NewWorkout = () => {
                 sets: 1,
                 reps: 1,
                 rest: {
-                    value: 1,
-                    unit: 'min'
+                    minutes: 0,
+                    seconds: 0
                 }
             }
         ])
         setExerciseInput('')
+    }
+
+    const clearExerciseInput = () => {
+        se
     }
 
     const decreaseReps = (id) => {
@@ -162,33 +168,21 @@ const NewWorkout = () => {
         )
     }
 
-    const handleRestChange = (id, value) => {
+    const handleRestChange = (exerciseId, unitId, value) => {
         setWorkoutExercises(
             workoutExercises.map((exercise) => {
-                return exercise.id === id
+                return exercise.id === exerciseId
                     ? {
                           ...exercise,
                           rest: {
-                              value:
-                                  Number.isNaN(Number(value)) || value === ''
-                                      ? 1
+                              [unitId]:
+                                  Number(value) > 59 || Number(value) < 0
+                                      ? 0
                                       : Number(value),
-                              unit: exercise.rest.unit
-                          }
-                      }
-                    : exercise
-            })
-        )
-    }
-    const handleUnitsChange = (id, value) => {
-        setWorkoutExercises(
-            workoutExercises.map((exercise) => {
-                return exercise.id === id
-                    ? {
-                          ...exercise,
-                          rest: {
-                              value: exercise.rest.value,
-                              unit: value
+                              [unitId === 'minutes' ? 'seconds' : 'minutes']:
+                                  unitId === 'minutes'
+                                      ? exercise.rest.seconds
+                                      : exercise.rest.minutes
                           }
                       }
                     : exercise
@@ -205,7 +199,7 @@ const NewWorkout = () => {
     const saveWorkout = async () => {
         //empty
     }
-
+    console.log(workoutExercises)
     return (
         <>
             <Helmet>
@@ -213,7 +207,10 @@ const NewWorkout = () => {
             </Helmet>
             <div className={styles.newWorkoutContainer}>
                 <form onSubmit={saveWorkout}>
-                    <h2>Novi trening</h2>
+                    <div className={styles.newWorkoutHeader}>
+                        <h2>Novi trening</h2>
+                        <div className={styles.headerSeparator}></div>
+                    </div>
                     <div className={styles.visibilityContainer}>
                         <label
                             htmlFor="private"
@@ -251,11 +248,13 @@ const NewWorkout = () => {
                         </label>
                     </div>
                     <div className={styles.workoutName}>
-                        <h3>Naziv treninga</h3>
+                        <h3>Naziv treninga:</h3>
                         <input
                             id="workoutName"
                             type="text"
                             value={workoutName}
+                            maxLength="50"
+                            placeholder="Unesite naziv treninga (max. 50 znakova)"
                             onChange={(e) => setWorkoutName(e.target.value)}
                         />
                     </div>
@@ -266,36 +265,46 @@ const NewWorkout = () => {
                             cols="70"
                             rows="7"
                             maxLength="500"
-                            placeholder="Maksimalno 500 znakova"
+                            placeholder="Unesite opis treninga (max. 500 znakova)"
                             value={workoutDesc}
                             onChange={(e) => setWorkoutDesc(e.target.value)}
                         />
                     </div>
                     <div className={styles.exerciseSelection}>
                         <h3>Odabir vježbi:</h3>
-                        <div className={styles.exerciseInput}>
+                        <div className={styles.exerciseInputContainer}>
+                            <div className={styles.exerciseInput}>
+                                <input
+                                    className={styles.exerciseInputField}
+                                    id="exercises"
+                                    list="exercisesList"
+                                    placeholder="Unesite naziv vježbe"
+                                    value={exerciseInput}
+                                    onChange={(e) =>
+                                        setExerciseInput(e.target.value)
+                                    }
+                                />
+                                <datalist id="exercisesList">
+                                    {allExercises.map((exercise) => (
+                                        <option key={exercise.exerciseId}>
+                                            {exercise.exerciseName} |{' '}
+                                            {
+                                                allCategories[
+                                                    exercise.category.categoryId
+                                                ]
+                                            }
+                                        </option>
+                                    ))}
+                                </datalist>
+                                <input
+                                    type="button"
+                                    value=" ⨉ "
+                                    className={styles.clearExerciseInput}
+                                    onClick={() => setExerciseInput('')}
+                                ></input>
+                            </div>
                             <input
-                                id="exercises"
-                                list="exercisesList"
-                                placeholder="Unesite naziv vježbe"
-                                value={exerciseInput}
-                                onChange={(e) =>
-                                    setExerciseInput(e.target.value)
-                                }
-                            />
-                            <datalist id="exercisesList">
-                                {allExercises.map((exercise) => (
-                                    <option key={exercise.exerciseId}>
-                                        {exercise.exerciseName} |{' '}
-                                        {
-                                            allCategories[
-                                                exercise.category.categoryId
-                                            ]
-                                        }
-                                    </option>
-                                ))}
-                            </datalist>
-                            <input
+                                className={styles.addExerciseButton}
                                 id="addExercise"
                                 type="button"
                                 value="Dodaj"
@@ -305,85 +314,128 @@ const NewWorkout = () => {
                         </div>
                     </div>
 
-                    {workoutExercises.map((exercise) => (
-                        <div key={exercise.id}>
-                            <div className={styles.separator}></div>
-                            <p>{exercise.name}</p>
-                            <p>Ponavljanja:</p>
-                            <input
-                                type="button"
-                                value="-"
-                                onClick={() => decreaseReps(exercise.id)}
-                                disabled={exercise.reps === 1}
-                            />
-                            <input
-                                type="text"
-                                value={exercise.reps}
-                                onChange={(e) =>
-                                    handleRepsChange(
-                                        exercise.id,
-                                        e.target.value
-                                    )
-                                }
-                            />
-                            <input
-                                type="button"
-                                value="+"
-                                onClick={() => increaseReps(exercise.id)}
-                            />
-                            <p>Serije:</p>
-                            <input
-                                type="button"
-                                value="-"
-                                onClick={() => decreaseSets(exercise.id)}
-                                disabled={exercise.sets === 1}
-                            />
-                            <input
-                                type="text"
-                                value={exercise.sets}
-                                onChange={(e) =>
-                                    handleSetsChange(
-                                        exercise.id,
-                                        e.target.value
-                                    )
-                                }
-                            />
-                            <input
-                                type="button"
-                                value="+"
-                                onClick={() => increaseSets(exercise.id)}
-                            />
-                            <p>Odmor:</p>
-                            <input
-                                type="text"
-                                value={exercise.rest.value}
-                                onChange={(e) =>
-                                    handleRestChange(
-                                        exercise.id,
-                                        e.target.value
-                                    )
-                                }
-                            />
-                            <select
-                                value={exercise.rest.unit}
-                                onChange={(e) =>
-                                    handleUnitsChange(
-                                        exercise.id,
-                                        e.target.value
-                                    )
-                                }
-                            >
-                                <option value="min">min</option>
-                                <option value="sek">sek</option>
-                            </select>
-                            <input
-                                type="button"
-                                value="Ukloni"
-                                onClick={() => removeExercise(exercise.id)}
-                            />
-                            <div className={styles.separator}></div>
-                        </div>
-                    ))}
+                    <div className={styles.workoutCurrentExercises}>
+                        <h3>Odabrane vježbe:</h3>
+                        {workoutExercises.length === 0 ? (
+                            <div className={styles.noExercisesYet}>
+                                <IconContext.Provider value={{ size: '80px' }}>
+                                    <BsEmojiFrown />
+                                </IconContext.Provider>
+
+                                <p>Prazno...</p>
+                                <p>(Vježbe koje dodate pojaviti će se ovdje)</p>
+                            </div>
+                        ) : (
+                            workoutExercises.map((exercise) => (
+                                <div
+                                    key={exercise.id}
+                                    className={styles.workoutExercise}
+                                >
+                                    <div className={styles.separator}></div>
+                                    <p>{exercise.name}</p>
+                                    <p>Ponavljanja:</p>
+                                    <input
+                                        className={styles.changeValueButton}
+                                        type="button"
+                                        value="-"
+                                        onClick={() =>
+                                            decreaseReps(exercise.id)
+                                        }
+                                        disabled={exercise.reps === 1}
+                                    />
+                                    <input
+                                        type="text"
+                                        value={exercise.reps}
+                                        onChange={(e) =>
+                                            handleRepsChange(
+                                                exercise.id,
+                                                e.target.value
+                                            )
+                                        }
+                                    />
+                                    <input
+                                        className={styles.changeValueButton}
+                                        type="button"
+                                        value="+"
+                                        onClick={() =>
+                                            increaseReps(exercise.id)
+                                        }
+                                    />
+                                    <p>Serije:</p>
+                                    <input
+                                        className={styles.changeValueButton}
+                                        type="button"
+                                        value="-"
+                                        onClick={() =>
+                                            decreaseSets(exercise.id)
+                                        }
+                                        disabled={exercise.sets === 1}
+                                    />
+                                    <input
+                                        className={styles.changeValueButton}
+                                        type="text"
+                                        value={exercise.sets}
+                                        onChange={(e) =>
+                                            handleSetsChange(
+                                                exercise.id,
+                                                e.target.value
+                                            )
+                                        }
+                                    />
+                                    <input
+                                        className={styles.changeValueButton}
+                                        type="button"
+                                        value="+"
+                                        onClick={() =>
+                                            increaseSets(exercise.id)
+                                        }
+                                    />
+                                    <p>Odmor:</p>
+                                    <fieldset>
+                                        <legend>MM:SS</legend>
+                                        <input
+                                            type="number"
+                                            id="minutes"
+                                            min="0"
+                                            max="59"
+                                            value={exercise.rest.minutes}
+                                            onChange={(e) =>
+                                                handleRestChange(
+                                                    exercise.id,
+                                                    e.target.id,
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
+                                        :
+                                        <input
+                                            type="number"
+                                            id="seconds"
+                                            min="0"
+                                            max="59"
+                                            value={exercise.rest.seconds}
+                                            onChange={(e) =>
+                                                handleRestChange(
+                                                    exercise.id,
+                                                    e.target.id,
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
+                                    </fieldset>
+                                    <input
+                                        type="button"
+                                        value="Ukloni"
+                                        onClick={() =>
+                                            removeExercise(exercise.id)
+                                        }
+                                    />
+                                    <div className={styles.separator}></div>
+                                </div>
+                            ))
+                        )}
+                    </div>
+
                     <div className={styles.buttonContainer}>
                         <NavLink to="/home" className={styles.returnButton}>
                             Odustani
